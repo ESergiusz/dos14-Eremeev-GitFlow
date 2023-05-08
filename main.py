dd624d94b502adc34a4876b77b948eeca668358e
Eremeev Sergey <saeremeev@gmail.com>
2023-05-08 21:32:15 +0300

    Homework 18

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import json
 import yaml
 
+roles = {}
+organisations = []
+users = []
+apps = []
+DB = {}
 class Permissions:
     def __init__(self, create=False, read=False, update=False, delete=False):
         self._create = create
@@ -33,22 +38,16 @@ class Permissions:
 
 
 class Role:
-    def __init__(self, name, **permissions):
+    def __init__(self, name, permissions):
         self._name = name
-        self._permissions = {}
-        for key, values in self._permissions.items():
-            self._permissions[key] = Permissions(**values)
-    # print(self._permissions)
+        self._role = {k: Permissions(**v) for k, v in permissions.items()}
+        # for key, values in permissions.items():
+        #     self._role[key] = Permissions(**values)
 
     def to_dict(self):
-
         perm = {}
-        for key, value in self._permissions.items():
-            print(key,value)
+        for key, value in self._role.items():
             perm[key] = value.to_dict()
-            # print(perm[key])
-            # print(f"value {perm[key] }")
-        # print(perm)
         return {
             'name': self._name,
             'permissions': perm
@@ -92,15 +91,15 @@ class User(Entity):
         self._date_of_birth = date_of_birth
 
     def to_dict(self):
-        fields = {
+        return {
             'entity_id': self._entity_id,
             'first_name': self._first_name,
+            'role': self._role.to_dict(),
             'last_name':  self._last_name,
             'fathers_name': self._fathers_name,
             'date_of_birth': self._date_of_birth,
-            'role': self._role.to_dict()
+
         }
-        return fields
 
     @property
     def first_name(self):
@@ -124,21 +123,21 @@ class User(Entity):
         return result
 
 class Organisation(Entity):
-    def __init__(self, entity_id, creation_date, unp, name, role):
+    def __init__(self, entity_id, role, creation_date, unp, name):
         super().__init__(entity_id, role)
         self._creation_date = creation_date
         self._unp = unp
         self._name = name
 
     def to_dict(self):
-        fields = {
+        return {
             'entity_id': self._entity_id,
-            'role': self._role,
+            'role': self._role.to_dict(),
             'creation_date': self._creation_date,
             'unp': self._unp,
             'name': self._name,
         }
-        return fields
+
     @property
     def creation_date(self):
         return self._creation_date
@@ -152,16 +151,22 @@ class Organisation(Entity):
         return self._name
 
 class App(Entity):
-    def __init__(self, entity_id, name, role=None):
+    def __init__(self, entity_id, name, role):
         super().__init__(entity_id, role)
         self._name = name
 
+    def to_dict(self):
+        fields = {
+            'entity_id': self._entity_id,
+            'name': self._name,
+            'role': self._role.to_dict()
+        }
+        return fields
     @property
     def name(self):
         return self._name
 
 
-# Функция по расчету возраста нашего пользователя.
 def age(birth, year=2023):
     result = year - birth
     return result
@@ -172,8 +177,8 @@ def new_user():
     last_name = input("Input last_name: ")
     fathers_name = input("Input fathers_name: ")
     date_of_birth = input("Input date_of_birth: ")
-    role_name = input("Input role_name: ")
-    max_id = max([int(user_item.entity_id) for user_item in users])
+    # role_name = input("Input role_name: ")
+    max_id = max([int(i.entity_id) for i in users])
     users.append(
         User(
             max_id + 1,
@@ -181,70 +186,77 @@ def new_user():
             last_name,
             fathers_name,
             date_of_birth,
-            roles[role_name],
+            roles[user_data['role']],
         )
     )
-
+    with open('DataBase.json', 'w') as convert_to_json:
+        json.dump(DB, convert_to_json, ensure_ascii=False, indent=2)
 
 def save_to_file(users, organisations):
-    DB = {"users": [], "organisations": []}
+    DB = {"users": [], "organisations": [], "apps": []}
     for user in users:
         DB["users"].append(user.to_dict())
     for organisation in organisations:
         DB["organisations"].append(organisation.to_dict())
+    for app in apps:
+        DB["apps"].append(app.to_dict())
     with open("DataBase.json", "w") as convert_to_json:
         json.dump(DB, convert_to_json, ensure_ascii=False, indent=4)
 
-
-# Создание объектов на основании данных
-with open("users.json", "r", encoding="utf8") as filejson:
-    users_data = json.loads(filejson.read())["Users"]
-users = []
-for user_data in users_data:
-    user = User(
-        int(user_data["entity_id"]),
-        user_data["first_name"],
-        user_data["last_name"],
-        user_data["fathers_name"],
-        int(user_data["date_of_birth"]),
-        user_data['role'],
-    )
-    users.append(user)
-
-with open("users.json", "r", encoding="utf8") as filejson:
-    organisation_data = json.loads(filejson.read())["Organisations"]
-organisations = []
-for organisation_data in organisation_data:
-    organization = Organisation(
-            int(organisation_data["entity_id"]),
-            organisation_data["role"],
-            organisation_data["creation_date"],
-            organisation_data["unp"],
-            organisation_data["name"],
-            )
-
-organisations.append(organization)
-with open("app.yaml", "r", encoding="utf8") as yamlfile:
-    apps_data = yaml.load(yamlfile, Loader=yaml.FullLoader)["Apps"]
-    apps = []
-    for app_data in apps_data:
-        app = App(int(app_data["entity_id"]), app_data["name"])
-    apps.append(app)
 with open("roles.yaml", "r", encoding="utf8") as yamlfile:
     roles_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    roles = {}
     for role_name, role_data in roles_data.items():
-        permissions = {}
-        for class_name, class_permissions in role_data.items():
-            # print(class_name, class_permissions)
-            permissions[class_name] = class_permissions #Permissions(**class_permissions)
-            # print(permissions)
-        roles[role_name] = Role(role_name, **permissions)
-for user in users:
-    user.role = roles[user_data["role"]]
+        roles[role_name] = Role(role_name, role_data)
 
-for app in apps:
-    app.role = roles[app_data["role"]]
-# print(roles)
-# new_user()
-save_to_file(users, organisations)
+with open("users.json", "r", encoding="utf8") as filejson:
+    users_data = json.loads(filejson.read())["Users"]
+
+    for user_data in users_data:
+        user = User(
+            int(user_data["entity_id"]),
+            user_data["first_name"],
+            user_data["last_name"],
+            user_data["fathers_name"],
+            user_data["date_of_birth"],
+            roles[user_data['role']]
+        )
+        users.append(user)
+
+with open("users.json", "r", encoding="utf8") as filejson:
+    organisations_data = json.loads(filejson.read())["Organisations"]
+
+    for org_data in organisations_data:
+        organization = Organisation(
+            int(org_data["entity_id"]),
+            roles[org_data["role"]],
+            org_data["creation_date"],
+            int(org_data["unp"]),
+            org_data["name"]
+        )
+        organisations.append(organization)
+
+with open("app.yaml", "r", encoding="utf8") as yamlfile:
+    apps_data = yaml.load(yamlfile, Loader=yaml.FullLoader)["Apps"]
+
+    for app_data in apps_data:
+        app = App(int(app_data["entity_id"]), app_data["name"], roles[app_data["role"]])
+        apps.append(app)
+
+command = input("What are you doing next? View DataBase (input view) "
+                "or add new user and view DataBase (input add) ? For quit input quit: \n")
+match command.split():
+    case ["quit"]:
+        print("Goodbye!")
+    case ["view"]:
+        save_to_file(users, organisations)
+        with open('DataBase.json','r') as f:
+            read_json = json.loads(f.read())
+        print(read_json)
+    case ["add"]:
+        new_user()
+        save_to_file(users, organisations)
+        with open('DataBase.json','r') as f:
+            read_json = json.loads(f.read())
+        print(read_json)
+    case _:
+        print(f"Sorry, I couldn't understand {command}. Goodbye")
